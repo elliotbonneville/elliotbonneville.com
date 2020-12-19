90fa151cbecfdcb54e4c8d157db0cfd7e01100f3
Elliot Bonneville <elliot.bonneville@gmail.com>
2020-12-19 00:14:44 -0500

    Build main layout

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-import { Layout, PostCard, Pagination } from '../components/common'
-import { MetaData } from '../components/common/meta'
+import { Layout, PostCard, Pagination } from '../components/common';
+import { MetaData } from '../components/common/meta';
 
 /**
 * Main index page (home page)
@@ -14,37 +14,37 @@ import { MetaData } from '../components/common/meta'
 *
 */
 const Index = ({ data, location, pageContext }) => {
-    const posts = data.allGhostPost.edges
+  const posts = data.allGhostPost.edges;
 
-    return (
-        <>
-            <MetaData location={location} />
-            <Layout isHome={true}>
-                <div className="container">
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
-                    <Pagination pageContext={pageContext} />
-                </div>
-            </Layout>
-        </>
-    )
-}
+  return (
+    <>
+      <MetaData location={location} />
+      <Layout isHome={true}>
+        <div className="container">
+          <section className="post-feed">
+            {posts.map(({ node }) => (
+              // The tag below includes the markup for each post - components/common/PostCard.js
+              <PostCard key={node.id} post={node} />
+            ))}
+          </section>
+          <Pagination pageContext={pageContext} />
+        </div>
+      </Layout>
+    </>
+  );
+};
 
 Index.propTypes = {
-    data: PropTypes.shape({
-        allGhostPost: PropTypes.object.isRequired,
-    }).isRequired,
-    location: PropTypes.shape({
-        pathname: PropTypes.string.isRequired,
-    }).isRequired,
-    pageContext: PropTypes.object,
-}
+  data: PropTypes.shape({
+    allGhostPost: PropTypes.object.isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  pageContext: PropTypes.object,
+};
 
-export default Index
+export default Index;
 
 // This page query loads all posts sorted descending by published date
 // The `limit` and `skip` values are used for pagination
@@ -62,4 +62,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    siteUrl: `http://elliotbonneville.com`, // Site domain. Do not include a trailing slash!
+  siteUrl: `http://elliotbonneville.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+  postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Elliot Bonneville`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Elliot Bonneville's Blog`, // This allows an alternative site description for meta data for pages.
+  siteTitleMeta: `Elliot Bonneville`, // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta: `Elliot Bonneville's Blog`, // This allows an alternative site description for meta data for pages.
 
-    shareImageWidth: 1000, // Change to the width of your default share image
-    shareImageHeight: 523, // Change to the height of your default share image
+  shareImageWidth: 1000, // Change to the width of your default share image
+  shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Elliot Bonneville`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
-}
+  shortTitle: `Elliot Bonneville`, // Used for App manifest e.g. Mobile Home Screen
+  siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+  backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+  themeColor: `#15171A`, // Used for Offline Manifest
+};
