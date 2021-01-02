180d7cd618d496b52ec8aa8a58b73394f2f06638
Elliot Bonneville <elliot.bonneville@gmail.com>
2021-01-01 21:43:28 -0500

    Update subscribe component

--- src/components/common/MailingListSignup.js
+++ src/components/common/MailingListSignup.js
@@ -16,22 +16,28 @@ const MailingListSignup = () => {
 
   return signedUp ? (
     <div className="my-8 text-lg text-gray-700 font-bold">
-      Thank you for signing up! Check your inbox for a confirmation email.
+      Thanks for signing up! 🥳 Check your inbox for a confirmation email (and
+      your spambox, if you have to).
     </div>
   ) : (
-    <form className="flex my-8 flex-col sm:flex-row" onSubmit={handleSubmit}>
-      <input
-        className="outline-none p-4 sm:mr-4 mb-4 sm:mb-0 flex-1 border border-gray-300 focus:border-gray-500 hover:border-gray-500 rounded-none"
-        placeholder="steve@apple.com"
-        type="email"
-        ref={emailInputRef}
-      />
-      <button
-        type="submit"
-        className="border border-gray-300 p-4 text-gray-400 hover:text-gray-500 hover:border-gray-500 bg-white"
-      >
-        Get Updates
-      </button>
+    <form className="my-8" onSubmit={handleSubmit}>
+      <div className="flex flex-col sm:flex-row">
+        <input
+          className="outline-none p-4 sm:mr-4 mb-4 sm:mb-0 flex-1 border border-gray-400 focus:border-gray-600 hover:border-gray-600 rounded-none"
+          placeholder="steve@apple.com"
+          type="email"
+          ref={emailInputRef}
+        />
+        <button
+          type="submit"
+          className="border border-gray-400 p-4 text-gray-500 hover:text-gray-600 hover:border-gray-600 bg-white"
+        >
+          Subscribe
+        </button>
+      </div>
+      <p className="text-gray-500 mt-2 text-sm">
+        I send a weekly roundup and the occasional announcement.
+      </p>
     </form>
   );
 };
